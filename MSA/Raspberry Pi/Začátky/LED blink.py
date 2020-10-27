ac11041ac8a2f5448bac61efb27d22a9014310ec
Roman Táborský <romcatab@seznam.cz>
2020-10-27 18:52:52 +0100

    Přidány poznámky

--- MSA/Raspberry Pi/Začátky/LED blink.py
+++ MSA/Raspberry Pi/Začátky/LED blink.py
@@ -4,8 +4,17 @@ from multiprocessing import Process
 
 from typing import List
 
+"""
+Program pro blikání několika LED s 1-sekundovými intervaly s možností přerušení a opětovaného startu činnosti 
+řízené přes tlačítko. Vyřešeno za pomoci několika procesů přes multiprocessing modul.
+"""
+
 
 def start(ledky2: List[LED]):
+    """
+    Funkce pro spuštění LED cyklu
+    :param ledky2: List obsahující objekty LED, které se mají spustit
+    """
     while True:
         for i2 in ledky2:
             i2.on()
@@ -13,24 +22,21 @@ def start(ledky2: List[LED]):
             i2.off()
 
 
-piny = (21, 20, 26, 16, 19, 13, 12, 6)
+piny = (21, 20, 26, 16, 19, 13, 12, 6)  # tuple pinů LED
 ledky = []
 tlacitko = Button(5)
 for i in piny:
-    ledky.append(LED(i))
+    ledky.append(LED(i))  # vytvoří list objektů LED z výše zmíněných pinů
 proces = Process(target=start, args=(ledky,))
 proces.start()
-run = True
 while True:
     tlacitko.wait_for_active()
-    if run:
+    if proces.is_alive():  # kontroluje zda proces již nebyl "ukončen"
         proces.kill()
         for i in ledky:
             i.off()
-        run = False
     else:
-        proces = Process(target=start, args=(ledky,))
+        proces = Process(target=start, args=(ledky,))  # objekt pro proces je potřeba znovu vytvořit (nelze spustit 2x)
         proces.start()
-        run = True
     tlacitko.wait_for_inactive()
-    sleep(.01)
+    sleep(.01)  # s mými tlačítky vznikaly nepřesnosti při rychlém zmáčknutí
