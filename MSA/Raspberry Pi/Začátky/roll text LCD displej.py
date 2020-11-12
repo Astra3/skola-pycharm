76e4924e50fcc4a555f1e25db3782fc8fe864a57
Roman Táborský <romcatab@seznam.cz>
2020-11-12 16:25:11 +0100

    Nový úkol do MSA

--- MSA/Raspberry Pi/Začátky/roll text LCD displej.py
+++ MSA/Raspberry Pi/Začátky/roll text LCD displej.py
@@ -1,17 +1,18 @@
 from time import sleep
 
-text = input("Zadejte text pro rolování: ")
-
+# text = input("Zadejte text pro rolování: ")
+text = "a"
 pocet_znaku = 14
 znak = "*"
-text += znak * 11
+text += znak * 8
 
 while True:
     for i in range(0, len(text)):
         if i + pocet_znaku >= len(text):
-            # FIXME když je před znakem hvězdička, tak znak se vypíše až poté, resp. nefunguje s malým počtem znak a
-            #  krátkým textem
-            print(f"\r{(text[i:] + text)[:pocet_znaku]}", end="", flush=True)
+            temp_text = text[i:] + text
+            while len(temp_text) < pocet_znaku:
+                temp_text += text
+            print(f"\r{temp_text[:pocet_znaku]}", end="", flush=True)
         else:
             print(f"\r{text[i:i + pocet_znaku]}", end="", flush=True)
-        sleep(.5)
+        sleep(.2)
