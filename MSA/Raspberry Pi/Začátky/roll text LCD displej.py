c49e652ddeb9ae2c05968bd89e8afd28474c6287
Roman Táborský <romcatab@seznam.cz>
2020-11-16 13:17:45 +0100

    Poznámky

--- MSA/Raspberry Pi/Začátky/roll text LCD displej.py
+++ MSA/Raspberry Pi/Začátky/roll text LCD displej.py
@@ -1,9 +1,17 @@
 from time import sleep
 
 # text = input("Zadejte text pro rolování: ")
+
+# definuje text
 text = "a"
+
+# počet znaků displeje
 pocet_znaku = 14
+
+# definuje znak co se dá mezi text
 znak = "*"
+
+# číslo na konci určuje kolik výše uvedených znaků bude vypsáno mezi text
 text += znak * 8
 
 while True:
