f7d8cd79cf1e0dcbfc3e3d48c6437bce45ae9e49
Roman Táborský <romcatab@seznam.cz>
2021-07-13 17:13:22 +0200

    úpravy pro distance

--- MSA/Raspberry Pi/Začátky/distance senzor.py
+++ MSA/Raspberry Pi/Začátky/distance senzor.py
@@ -1,10 +1,12 @@
 import time
 
-from gpiozero import DistanceSensor, LED
+from gpiozero import DistanceSensor, LED, PWMLED
+import numpy as np
 
 dist = DistanceSensor(19, 13, max_distance=4)
-led = LED(26)
+led = PWMLED(16)
 
-while True:
-    print(dist.distance)
-    time.sleep(.2)
+try:
+    led.pulse(.5, .5, background=False)
+except KeyboardInterrupt:
+    led.close()
